dbd7e389a2c5800116429bc67973e5acfc8bbccf
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2020-06-02 18:42:17 -0300

    criando a página de cadastro de coletas, 1ª parte do formulário ( dados da entidade) finalizada (com utilização da base de dados do IBGE para dar as opções de UFs e Municípios da UF selecionada)

---
+++ scripts/intro.js
@@ -0,0 +1,76 @@
+//coments
+// document.write("Hello")
+
+// let myvar = "World"
+
+// const myconst = "Hello"
+
+// document.write(myconst + myvar)
+
+//string
+
+// const s1 = "isso é uma string"
+// const s2 = 'string também'
+// const s3 = `outra vez string`
+
+//number
+// const n1 = 1
+// const n2 = 12
+
+// document.write(n1 + n2)
+
+//boolean - true or false
+
+// const bTrue = true
+// const bFalse = false
+
+// document.write(bFalse)
+
+//objeto possuem propriedades e funcionalidades
+
+// const pessoa = {
+//   altura: "1,60m",
+//   idade: 31,
+//   solteiro: true,
+//   correr() {
+//     document.write("executar uma grande lógica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+//Array - Vetores
+
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+//Funções - Funcionalidades - Atalhos - Reuso
+
+//registrar função
+// function sayMyName(name) {
+//   document.write(name)
+// }
+
+// sayMyName("Rodrigo")
+// sayMyName("Senna")
+// sayMyName("Santos")
+
+//condicional
+
+// const notaFinal = 7
+
+// if( notaFinal < 5 ) {
+//   //caminho 1
+//   document.write("Reprovado")
+// } else {
+//   //caminho 2
+//   document.write("Aprovado")
+// }
+
+//loop - repetições
+
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
+
